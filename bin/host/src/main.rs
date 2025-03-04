aee5e04dc5dd436eee0639b16efb1cdf12206788
leruaa <aurelien.catinon@gmail.com>
2025-03-04 02:59:20 -0800

    fix: feedbacks

--- bin/host/src/main.rs
+++ bin/host/src/main.rs
@@ -55,7 +55,8 @@ async fn main() -> eyre::Result<()> {
             block_execution_strategy_factory,
             persist_execution_report,
             config,
-        )?;
+        )
+        .await?;
 
         executor.execute(block_number).await?;
     } else {
@@ -70,7 +71,8 @@ async fn main() -> eyre::Result<()> {
             block_execution_strategy_factory,
             persist_execution_report,
             config,
-        )?;
+        )
+        .await?;
 
         executor.execute(block_number).await?;
     }
